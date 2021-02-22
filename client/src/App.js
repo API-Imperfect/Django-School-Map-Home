1ad029ecba5211639471eb676e1e20b61a73675d
Ogilo <alphaogilo@gmail.com>
2021-02-22 12:46:09 +0200

    chore: setup react with docker and nginx

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+   return (
+      <div className="App">
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+               Edit <code>Alpha Ogilo</code> and save to reload!
+            </p>
+         </header>
+      </div>
+   );
+}
+
+export default App;
